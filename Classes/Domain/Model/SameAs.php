c4ed8d0ab1d0f40b7b83f25e66efa7e19b21345c
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2023-10-09 14:32:02 +0200

    Add SameAs class

---
+++ Classes/Domain/Model/SameAs.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+# This file is part of the extension CHF Base for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+namespace Digicademy\CHFBase\Domain\Model;
+
+use TYPO3\CMS\Extbase\Annotation\Validate;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+/**
+ * Model for same as
+ */
+class SameAs extends AbstractEntity
+{
+    /**
+     * Whether the record should be visible or not
+     * 
+     * @var bool
+     */
+    #[Validate([
+        'validator' => 'Boolean',
+    ])]
+    protected bool $hidden = false;
+
+    /**
+     * External web address to identify an entity across the web
+     * 
+     * @var string
+     */
+    #[Validate([
+        'validator' => 'Url',
+    ])]
+    protected string $uri = '';
+
+    /**
+     * Construct object
+     *
+     * @param string $uri
+     * @return SameAs
+     */
+    public function __construct(string $uri)
+    {
+        $this->setUri($uri);
+    }
+
+    /**
+     * Get hidden
+     *
+     * @return bool
+     */
+    public function getHidden(): bool
+    {
+        return $this->hidden;
+    }
+
+    /**
+     * Set hidden
+     *
+     * @param bool $hidden
+     */
+    public function setHidden(bool $hidden): void
+    {
+        $this->hidden = $hidden;
+    }
+
+    /**
+     * Get URI
+     *
+     * @return string
+     */
+    public function getUri(): string
+    {
+        return $this->uri;
+    }
+
+    /**
+     * Set URI
+     *
+     * @param string $uri
+     */
+    public function setUri(string $uri): void
+    {
+        $this->uri = $uri;
+    }
+}
+
+?>
