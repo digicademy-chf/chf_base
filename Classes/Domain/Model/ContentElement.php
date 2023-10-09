20bcea29ef419328c2cf02f3015afad461fc30d1
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2023-10-09 14:36:13 +0200

    Add ContentElement class

---
+++ Classes/Domain/Model/ContentElement.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+# This file is part of the extension CHF Base for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+namespace Digicademy\CHFBase\Domain\Model;
+
+use TYPO3\CMS\Extbase\Annotation\Validate;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+/**
+ * Model for content elements
+ */
+class ContentElement extends AbstractEntity
+{
+    /**
+     * Whether the record should be visible or not
+     * 
+     * @var bool
+     */
+    #[Validate([
+        'validator' => 'Boolean',
+    ])]
+    protected bool $hidden = false;
+
+    /**
+     * Name of the content element
+     * 
+     * @var string
+     */
+    #[Validate([
+        'validator' => 'String',
+    ])]
+    protected string $header = '';
+
+    /**
+     * Body text of the content element
+     * 
+     * @var string
+     */
+    #[Validate([
+        'validator' => 'String',
+    ])]
+    protected string $bodytext = '';
+
+    /**
+     * Construct object
+     *
+     * @param string $header
+     * @param string $bodytext
+     * @return Content
+     */
+    public function __construct(string $header, string $bodytext)
+    {
+        $this->setHeader($header);
+        $this->setBodytext($bodytext);
+    }
+
+    /**
+     * Get hidden
+     *
+     * @return bool
+     */
+    public function getHidden(): bool
+    {
+        return $this->hidden;
+    }
+
+    /**
+     * Set hidden
+     *
+     * @param bool $hidden
+     */
+    public function setHidden(bool $hidden): void
+    {
+        $this->hidden = $hidden;
+    }
+
+    /**
+     * Get header
+     *
+     * @return string
+     */
+    public function getHeader(): string
+    {
+        return $this->header;
+    }
+
+    /**
+     * Set header
+     *
+     * @param string $header
+     */
+    public function setHeader(string $header): void
+    {
+        $this->header = $header;
+    }
+
+    /**
+     * Get bodytext
+     *
+     * @return string
+     */
+    public function getbodytext(): string
+    {
+        return $this->bodytext;
+    }
+
+    /**
+     * Set bodytext
+     *
+     * @param string $bodytext
+     */
+    public function setBodytext(string $bodytext): void
+    {
+        $this->bodytext = $bodytext;
+    }
+}
+
+?>
