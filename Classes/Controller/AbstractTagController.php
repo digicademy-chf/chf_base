0a2a38501d860ebbfb5278061548f6c09fe601ba
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2023-10-19 11:46:59 +0200

    Fully implement AbstractTag, part 1

---
+++ Classes/Controller/AbstractTagController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+# This file is part of the extension CHF Base for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+namespace Digicademy\CHFBase\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Digicademy\CHFBase\Domain\Model\AbstractTag;
+use Digicademy\CHFBase\Domain\Repository\AbstractTagRepository;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+/**
+ * Controller for AbstractTag
+ */
+class AbstractTagController extends ActionController
+{
+    private AbstractTagRepository $abstractTagRepository;
+
+    public function injectAbstractTagRepository(AbstractTagRepository $abstractTagRepository): void
+    {
+        $this->abstractTagRepository = $abstractTagRepository;
+    }
+
+    public function indexAction(): ResponseInterface
+    {
+        $this->view->assign('abstractTags', $this->abstractTagRepository->findAll());
+        return $this->htmlResponse();
+    }
+
+    public function showAction(AbstractTag $abstractTag): ResponseInterface
+    {
+        $this->view->assign('abstractTag', $abstractTag);
+        return $this->htmlResponse();
+    }
+}
+
+?>
