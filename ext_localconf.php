52d22b97f0c3cf0c6507af127c81ec3a3c31d79f
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-01-29 18:39:28 +0100

    Add PHP security

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,11 @@
 <?php
+defined('TYPO3') or die();
 
 # This file is part of the extension CHF Base for TYPO3.
 #
 # For the full copyright and license information, please read the
 # LICENSE.txt file that was distributed with this source code.
 
-
-// Prevent script from being called directly
-defined('TYPO3') or die();
 
 // Register content for this extension
 (function($extKey='chf_base') {
